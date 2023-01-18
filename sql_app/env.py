5f4bfece8069d21854699c54f62508f558e848cd
joongwon <winterdawnlight@snu.ac.kr>
2023-01-19 01:25:34 +0900

    initial commit

---
+++ sql_app/env.py
@@ -0,0 +1,2 @@
+from .database import Base
+target_metadata = Base.metadata
---
+++ sql_app/main.py
@@ -0,0 +1,66 @@
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException, UploadFile
+from sqlalchemy.orm import Session
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+from . import crud, schemas, models
+from .database import SessionLocal, engine
+from pydantic import parse_obj_as
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return schemas.User.from_orm(crud.create_user(db, user))
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip, limit)
+    return parse_obj_as(List[schemas.User], users)
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return schemas.User.from_orm(db_user)
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_item = crud.create_user_item(db, item, user_id)
+    return schemas.Item.from_orm(db_item)
+
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip, limit)
+    return parse_obj_as(List[schemas.Item], items)
